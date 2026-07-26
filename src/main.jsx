4b4b1d926ed53f310e00735724ba2c63fe5fedbc
Gilson Gil <gilsongillazaro@gmail.com>
2026-07-26 19:00:17 +0100

    adicao da vercel analitics

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ToastContainer } from 'react-toastify'
-import { FuncaoProvider } from './Funcao.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ToastContainer } from "react-toastify";
+import { FuncaoProvider } from "./Funcao.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-  <FuncaoProvider>
+    <FuncaoProvider>
+      <Analytics />
       <App />
-    <ToastContainer/>
-  </FuncaoProvider>
+      <ToastContainer />
+    </FuncaoProvider>
   </StrictMode>,
-)
+);
